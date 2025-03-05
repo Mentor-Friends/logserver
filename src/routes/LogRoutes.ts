dd8a4d284501174dab1856afa47cbb9db558c022
sujanpoudel <sujanpoudel@mentorfriends.com>
2025-03-05 14:26:34 +0545

    update file empty mechanism and temp file for anomaly detection

--- src/routes/LogRoutes.ts
+++ src/routes/LogRoutes.ts
@@ -1,11 +1,12 @@
 // src/routes/LogRoutes.ts
 import express from 'express';
 import { addLogs, getApplicationLogs, getPackageLogs } from '../controllers/LogController';
+import verifyRequestToken from '../middlewares/verifyRequestToken';
 
 const router = express.Router();
 
 // Define routes
-router.post('/logger', addLogs)
+router.post('/logger',verifyRequestToken, addLogs)
 router.get('/package-logs', getPackageLogs);
 router.get('/application-logs', getApplicationLogs);
 router.get('/schema-query', )
--- src/services/logger.service.ts
+++ src/services/logger.service.ts
@@ -1,6 +1,7 @@
 import fs from 'fs'
 import path from 'path'
 import zlib from 'zlib'
+import { AnomalyService } from './anomaly/anomaly.service'
 
 export class LogService {
   private static mftsccs: string = 'mftsccs'
@@ -42,7 +43,9 @@ export class LogService {
     try {
       // Add logs to the appropriate user-specific log folder
       if (logType === this.mftsccs) {
-        this.saveLogToFile(userId, this.mftsccs, logEntry)
+        this.saveLogToFile(userId, this.mftsccs, logEntry);
+        // Send logs for anomaly detection
+        AnomalyService.addLogToAnomaly(userId, this.mftsccs, logEntry);
       } else if (logType === this.app) {
         this.saveLogToFile(userId, this.app, logEntry)
       }
@@ -73,7 +76,7 @@ export class LogService {
     // Determine the correct file name and path
     const fileName = `${logType}log_user_${userId}.log`
     const filePath = path.join(userLogDir, fileName)
-    console.log("this is the file", filePath);
+    // console.log("this is the file", filePath);
     if (!logEntry) {
       return
     }
@@ -122,7 +125,11 @@ export class LogService {
         fs.writeFileSync(zipFileName, gzippedContent)
   
         // Clear the original file
-        fs.writeFileSync(filePath, JSON.stringify([]))
+        fs.truncate(filePath, 0, (err) => {
+          if(err){
+            console.error(`Error truncating file : ${err}`);
+          }
+        });
       }
     }
     catch(ex){
