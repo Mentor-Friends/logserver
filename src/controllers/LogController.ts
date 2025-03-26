ae5da2a6bb8e4508ec39714daf2efa58a94087af
sujanpoudel <sujanpoudel@mentorfriends.com>
2025-03-26 13:34:50 +0545

    add log

--- src/controllers/LogController.ts
+++ src/controllers/LogController.ts
@@ -71,7 +71,8 @@ export async function addLogs(req:any, res:any): Promise<void>{
       userId = await decodedTokenForUserId(token) ?? 998;
       //  const userId = req.user?.userId ?? 998;
     }
-
+    console.log(`Log of : ${userId}`);
+    
     // Check for logType and logData
     const { logType, logData } = req.body
     if (!logType || !logData) {
