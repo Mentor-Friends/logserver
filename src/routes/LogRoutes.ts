aa8b3c217bb5ef6ff0da1122810a6849b523232e
nischalbhandari12 <nischal.bhandari@mentorfriends.com>
2025-08-19 12:12:53 +0545

    Added health check

--- src/routes/LogRoutes.ts
+++ src/routes/LogRoutes.ts
@@ -1,11 +1,13 @@
 // src/routes/LogRoutes.ts
 import express from 'express';
 import { addLogs, getApplicationLogs, getPackageLogs } from '../controllers/LogController';
+import { healthCheck } from '../controllers/HealthCheckController';
 
 const router = express.Router();
 
 // Define routes
 router.post('/logger', addLogs)
+router.get('/check', healthCheck)
 router.get('/package-logs', getPackageLogs);
 router.get('/application-logs', getApplicationLogs);
 router.get('/schema-query', )
